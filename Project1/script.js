f9ee6756b38b5ccf7ff89be57250a118e167f1a6
Nitish <raaznitish9931.com>
2024-07-06 17:42:00 +0530

    Coffee

---
+++ Project1/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
